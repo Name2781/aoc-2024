4239fbce6914a362e1cdf4b8fe5dc5becd8ba3c4
Name2781 <98187590+Name2781@users.noreply.github.com>
2024-12-01 20:42:54 -0500

    Level 1 done

---
+++ Level1/src/Program.cs
@@ -0,0 +1,42 @@
+﻿public class Level1
+{
+    public static void Main(string[] args)
+    {
+        var lines = File.ReadAllLines(args[0]);
+        
+        List<int> Map1 = [];
+        List<int> Map2 = [];
+
+        foreach (var line in lines)
+        {
+            var split = line.Split("   ");
+
+            Map1.Add(int.Parse(split[0]));
+            Map2.Add(int.Parse(split[1]));
+        }
+
+        if (args[1] == "p1")
+        {
+            Map1.Sort();
+            Map2.Sort();
+
+            var total = 0;
+            for (int i = 0; i < Map1.Count; i++)
+            {
+                total += Math.Abs(Map1[i] - Map2[i]);
+            }
+
+            Console.WriteLine(total);
+        }
+        else if (args[1] == "p2")
+        {
+            var similarity = 0;
+            for (int i = 0; i < Map1.Count; i++)
+            {
+                similarity += Map1[i] * Map2.Where(x => x == Map1[i]).Count();
+            }
+
+            Console.WriteLine(similarity);
+        }
+    }
+}
